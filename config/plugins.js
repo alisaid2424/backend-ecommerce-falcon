ecbab5d10c536daf5cab6ca45b9f2bd5b6dd08d6
ali said <aliasy19932017@gmail.com>
2025-04-03 11:49:21 +0200

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUD_NAME"),
+        api_key: env("API_KEY"),
+        api_secret: env("API_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
